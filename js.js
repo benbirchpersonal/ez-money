6e4a97955d78829706fbfa49f89c0efd49f2f6d1
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2022-01-17 10:22:03 +0000

    Update js.js

--- js.js
+++ js.js
@@ -17,4 +17,24 @@ function shortenClicked(){
   		});
 }
 
+function randstr(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+function shorten(){
+  if($('#cst').is(':checked')){
+    $("#shortenedLink").text( "quay.group" + "/" + $("#customLinkBox").val());
+  }
+  else{
+    $("#shortenedLink").text( "quay.group" + "/" + randstr(5));
+  }
+}
+
 
